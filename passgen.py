d4ea07c712632470c9b6ac8e98f943c322bbaaad
bfcy <commits@mailfence.com>
2020-03-11 22:43:17 -0700

    refactored code

--- passgen.py
+++ passgen.py
@@ -5,24 +5,49 @@ instead of random letters and numbers.
 
 import random
 
+# I/P: filehandle to a wordlist text file
+# O/P: returns a list of words and random word from the list
+def parsefile(filehandle):
+	linecount = 0
+	lines = filehandle.readlines()
+	linecount = len(lines)
 
-complexityLevel = 2
+	return lines, linecount
+
+# I/P: wordList and numOfLines
+def randomWord(wordList, numOfLines):
+	randomline = randomNum(numOfLines)
+
+	wordsPerLine = len(wordList[randomline].split())
+
+	if (wordsPerLine == 2):
+		return(wordList[randomline].split()[1])
+	elif (wordsPerLine == 1): 	
+		return(wordList[randomline])
 
 
-#fhand = open('words/word-list-65635.txt')
-fhand = open('words/eff_large_wordlist.txt')
-#fhand = open('words/eng_words.txt')
+# I/P: maximum int
+# O/P: a random number within that range
+def randomNum(maxVal):
+	rnum=random.random()
+	rlinenum = int(rnum*maxVal)
+	return(rlinenum)
 
-contents = fhand.readlines()
 
-counts = 0
-for line in contents:
-	counts = counts + 1
 
-print(counts)
-rnum=random.random()
-print(rnum)
-rlinenum = int(rnum*counts)
-print(rlinenum)
+def main():
 
-print(contents[rlinenum].split()[1])
+	#fhand = open('words/word-list-65635.txt')
+	fhand = open('words/eff_large_wordlist.txt')
+	#fhand = open('words/eng_words.txt')
+	passphrasewords = int(input("Number of words in pass phrase: "))
+
+	wordList, numOfLines = parsefile(fhand)
+
+	for passphraseword in range(passphrasewords):
+		rword = randomWord(wordList, numOfLines)
+		print(rword)
+
+
+if __name__ == '__main__':
+	main()
