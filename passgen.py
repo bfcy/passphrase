b92f19381b1b6b8a3ff0f93c2f844ae546d4d99f
bfcy <commits@mailfence.com>
2020-03-18 22:01:34 -0700

    test input for invalid entry

--- passgen.py
+++ passgen.py
@@ -4,7 +4,6 @@ Author       : BFCY
 Description  : This program creates passwords using words
              : instead of random letters and numbers.
 '''
-
 import random
 
 # I/P: filehandle to a wordlist text file
@@ -28,7 +27,7 @@ def randomWord(wordList, numOfLines):
 		return(wordList[randomline])
 
 
-# I/P: maximum int
+# I/P: number of words in the wordlist file
 # O/P: a random number within that range
 def randomNum(maxVal):
 	rnum=random.random()
@@ -42,13 +41,28 @@ def main():
 	#fhand = open('words/word-list-65635.txt')
 	fhand = open('words/eff_large_wordlist.txt')
 	#fhand = open('words/eng_words.txt')
-	passphrasewords = int(input("Number of words in pass phrase: "))
-
+	
 	wordList, numOfLines = parsefile(fhand)
+	
+	print("Pass Phrase Generator")
 
-	for passphraseword in range(passphrasewords):
-		rword = randomWord(wordList, numOfLines)
-		print(rword)
+	passphrasewords = input("Number of words: ")
+	testinput = passphrasewords
+	
+	try:
+		testinput = int(passphrasewords)
+		if testinput < 0:
+			print('positive integers only')
+	except ValueError:
+		try:
+			testinput = float(passphrasewords)
+			print("ValueError: Positive integers only")
+		except ValueError:
+			print("TypeError: Please enter a nonnegative number")
+	else:
+		for passphraseword in range(int(passphrasewords)):
+			rword = randomWord(wordList, numOfLines)
+			print(rword)
 
 
 if __name__ == '__main__':
